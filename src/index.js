6c7a17d40deab1c4a043d236e4ae8f0fdacc391e
Maxym <maks064040@gmail.com>
2020-09-25 05:50:58 +0300

    remove node

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./styles/index.scss";
 import { Provider } from "react-redux";
 import store from "./store";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
-  </Provider>,
+    </Provider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
