5a14c4b65a49b6155044bf32f22242e7ac7e074b
Maxym <maks064040@gmail.com>
2020-09-25 06:02:41 +0300

    build

---
+++ build/precache-manifest.6ac45046422c598f612d08554b31836d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc13cc6bf15f828960ba08db0189b2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e832b8d1f4392c494f13",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "1cbf005a78fac2d56433",
+    "url": "/static/css/main.679ea83a.chunk.css"
+  },
+  {
+    "revision": "e832b8d1f4392c494f13",
+    "url": "/static/js/2.16ca2037.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.16ca2037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cbf005a78fac2d56433",
+    "url": "/static/js/main.e445e15f.chunk.js"
+  },
+  {
+    "revision": "402f05c59b4e2a57dec5",
+    "url": "/static/js/runtime-main.d1310a82.js"
+  },
+  {
+    "revision": "c1b2293586edb83bcd635cdb4e299109",
+    "url": "/static/media/Logo original RGB.c1b22935.jpg"
+  },
+  {
+    "revision": "37a75b54218416da18cdf1fa0650c569",
+    "url": "/static/media/background.37a75b54.jpg"
+  }
+]);
